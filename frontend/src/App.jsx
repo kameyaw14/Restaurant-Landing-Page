fc964df80aea1cd5d12f351ee8d3c5f932350870
chiefgit <kameyaw14@gmail.com>
2025-01-07 12:54:54 +0000

    tweaks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     backroundRepeat: "no-repeat",
     backgroundSize: "cover",
     backgroundPosition: "center",
-    backgroundAttachment: "fixed",
+    backgroundAttachment: "scroll",
   };
 
   return (
@@ -18,6 +18,65 @@ const App = () => {
       <div className="container">
         <div style={bgStyle} className="rounded-3xl">
           <Hero />
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur
+          unde, minima esse quod totam consequatur ducimus saepe nulla vel id
+          obcaecati modi quos cupiditate nemo corporis fugiat provident illum?
+          Sunt facere cumque dignissimos molestiae nesciunt cum quod aut placeat
+          necessitatibus nisi illum officiis laborum voluptatibus ea, veritatis
+          iure deserunt numquam officia harum doloremque libero? Similique,
+          eligendi! Veniam possimus nemo distinctio facilis impedit, blanditiis
+          quod sint repellendus aut nobis quis, inventore ipsa saepe! Aspernatur
+          laboriosam illum, minima unde consectetur rem excepturi molestias ipsa
+          est aperiam, optio inventore exercitationem quia soluta iusto incidunt
+          tenetur totam, consequatur quasi quo ipsam voluptatibus? Excepturi
+          doloribus beatae laudantium porro repellendus adipisci. Excepturi,
+          veritatis recusandae! Magnam expedita vitae officiis praesentium, quae
+          saepe minus hic nemo voluptatibus nobis ullam voluptate illo eius
+          error facere iure molestias in dolore ad natus provident nisi. Earum,
+          distinctio mollitia! Voluptates tenetur iste cum, neque voluptas
+          architecto nobis magnam ex consequatur blanditiis corrupti vel minima
+          facere itaque quidem laborum, tempore asperiores. Eaque voluptatum
+          nisi, reiciendis incidunt quisquam magnam sit blanditiis voluptatibus
+          nam explicabo necessitatibus quasi adipisci sapiente est labore? Illum
+          illo adipisci, ad consequuntur doloremque corrupti voluptatibus at
+          sequi excepturi porro exercitationem, quibusdam doloribus commodi sunt
+          ut! Sunt, atque incidunt? Aliquid in corrupti accusantium unde nihil.
+          Expedita molestiae esse saepe eaque facilis. Voluptatum, nihil in
+          dolor ut cum animi harum est consequuntur sapiente eius dolorem
+          quidem, officia obcaecati, atque suscipit amet recusandae autem vero
+          reiciendis. Distinctio quasi, voluptatum eligendi reprehenderit
+          doloribus officia accusantium aliquam exercitationem ipsa? Facilis
+          excepturi molestias similique in dolores fugiat et. Consectetur
+          officiis adipisci eius libero rem error architecto quod exercitationem
+          ea amet, dicta provident neque excepturi expedita voluptates ducimus
+          veritatis cumque tempore illo incidunt, hic illum. Modi, quos! Eius
+          pariatur fugit magnam reprehenderit consequuntur. Cum praesentium ut
+          accusamus excepturi inventore tenetur est animi natus nemo culpa!
+          Facere rem tempore ex ullam corrupti magnam dicta voluptate itaque
+          quam? Animi voluptates optio repellat tenetur tempore, aliquid,
+          recusandae unde saepe asperiores labore nostrum quisquam cupiditate
+          sequi facere quibusdam! Nulla, tempore eum! Non nostrum cum facere
+          sunt incidunt quae quia suscipit laborum veniam, praesentium iure unde
+          minus hic corporis. Tempora iste in nulla magni ad nihil optio debitis
+          similique, qui possimus! Labore placeat magnam molestias optio amet
+          rerum nisi quisquam? Et commodi dignissimos vel placeat deleniti
+          facere itaque perferendis necessitatibus quasi eaque? Illo suscipit
+          velit maiores vitae molestias recusandae, temporibus quod, earum,
+          impedit mollitia perferendis corporis ipsum fugit cumque? Facere
+          obcaecati, minima enim voluptates, exercitationem fugiat tempora totam
+          quis mollitia eveniet saepe libero ducimus sed! Voluptatem hic quia ad
+          atque molestias eveniet aut optio sed impedit, iste maxime minima
+          nesciunt eius voluptate enim totam, laudantium consequatur. Minus
+          expedita vel nostrum voluptas nesciunt excepturi voluptatibus quam
+          nulla possimus, ut velit, cum id itaque animi amet totam placeat quo
+          dolorem? Voluptatum animi laboriosam quaerat corrupti ipsa laudantium
+          esse, modi porro autem dolor provident nesciunt rerum doloremque
+          dolores perspiciatis inventore eius expedita consequatur debitis
+          corporis. Ex minima quia quam, ullam pariatur enim ipsam. Eaque
+          consequatur placeat optio dolorum, ea sint, omnis, explicabo odio
+          architecto in ducimus!
         </div>
       </div>
     </div>
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,20 +1,45 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { SlideLeft, SlideRight } from "../utils/animation";
 
 const Hero = () => {
+  const primaryBtn =
+    "bg-white rounded-[50px] text-gray-500 font-semibold py-[2vw] px-[4vw] sm:py-[1vw] sm:px-[3vw] md:py-[1rem] md:px-[2rem]  hover:!scale-110 duration-300 text-[max(1.2vw,14px)] sm:text-[max(1.5vw,16px)] md:text-[1rem] lg:text-[1.2rem]";
+
   return (
-    <div className="Header relative h-[34vw] my-[30px] mx-auto">
-      <div className="header-contents absolute flex flex-col items-start gap-[1.5vw] max-w-[50%] bottom-[10%] left-[6vw]">
-        <h2 className="font-bold text-white text-[max(4.5vw,22px)] [text-shadow:2px_2px_4px_green]">
+    <div className="Header relative h-[40vh] sm:h-[50vh] md:h-[60vh] lg:h-[70vh] my-[20px] mx-auto ">
+      <div className="header-contents  absolute flex flex-col items-start gap-[4%] sm:gap-[3%] md:gap-[2.5%] lg:gap-[2%] max-w-[90%] sm:max-w-[80%] md:max-w-[70%] lg:max-w-[50%] bottom-[10%] left-[5%]">
+        <motion.h2
+          variants={SlideRight(1)}
+          initial="hidden"
+          animate="visible"
+          className="font-bold text-white text-[max(6vw,22px)]  sm:text-[5vw] md:text-[4vw] lg:text-[3.5vw] xl:text-[3vw] [text-shadow:2px_2px_4px_green] leading-tight"
+        >
           Lorem ipsum dolor sit amet.
-        </h2>
-        <p className="text-white text-[1vw] [text-shadow:2px_2px_4px_black]">
-          Lorem ipsumw dolor sit amet consectetur adipisicing elit. Cum magnam
+        </motion.h2>
+        <motion.p
+          variants={SlideLeft(1.3)}
+          initial="hidden"
+          animate="visible"
+          className="mt-3 text-white text-[max(3.5vw,14px)] sm:text-[3vw] md:text-[2.5vw] lg:text-[1.5vw] xl:text-[1vw] [text-shadow:2px_2px_4px_black] leading-snug"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum magnam
           quod culpa incidunt ut quos repudiandae architecto cumque cupiditate
           ipsum.
-        </p>
-        <button className="border-none text-gray-500 py-[1vw] px-[2.3vw] bg-white text-[max(1vw,13px)] rounded-[50px]">
-          View Menu
-        </button>
+        </motion.p>
+
+        <motion.div
+          variants={SlideRight(1.6)}
+          initial="hidden"
+          animate="visible"
+          className="flex justify-center items-center gap-6 sm:gap-8 lg:gap-10 mt-6 md:mt-8 lg:mt-10"
+        >
+          <button
+            className={`${primaryBtn} flex items-center gap-2 text-[1rem] sm:text-[1.1rem] md:text-[1.2rem] lg:text-[1.3rem] mt-4`}
+          >
+            View Menu
+          </button>
+        </motion.div>
       </div>
     </div>
   );
